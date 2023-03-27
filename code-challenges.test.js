0f5145309fbef89e7a7c8099727ea9eeb59bcb3a
iraholmes <ira.holmes63@gmail.com>
2023-03-27 12:31:02 -0700

    Week 5 assessment complete, question 3b almost complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,8 +11,20 @@
 // Run the file with the following command: $ yarn jest
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
-
+// Create an expect statement that takes in all 3 variables
+// create a new parameter named "newWord"
+// decide on what method will be used to convert a to 4, e to 3, i to 1, and o to 0
+// try using the .includes? method 
 // a) Create a test with expect statements using the variables provided.
+// The .include method did not work so, instead I looked into a new method called regex
+describe("codex", () => {
+    it("The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o', to 0", () => {
+      expect(codex(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codex(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(codex(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+//First Test:  ReferenceError:  codex is not defined
 
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
@@ -22,23 +34,117 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+// const codex = (shhh) => {
+// const  letterChange = shhh.replace("a","4"), ("e","3"), ("i","1"), ("o","0")  
+//   return letterChange
+  
+// }
+//  const letters = shhh.includes("a, e, i, o")
 
+// const codex = (shh) => {
+//   const newCode = {
+//    letter:"a",  number: "4",
+//    letter:"e",  number: "3",
+//    letter:"i",  number: "1",
+//    letter:"o",  number: "0"}
+//   const neWord = shh.includes(newCode)
+//   if (neWord.includes("a")) {
+//     return neWord.replaceAll("a", "4") 
+
+//   } else if (neWord.includes("e")) {
+//           return neWord.replaceAll("e", "3")
+//   } else if (neWord.includes("i")){
+//           return neWord.replaceAll("i", "1")
+//   } else if (neWord.includes("o")) 
+//           return
+//           neWord.replaceAll("o", "0")
+  
+// }
+const codex = (string) => {
+    return string.replace(/a/gi, 4).replace(/[eE]/gi, 3).replace(/i/gi, 1).replace(/o/gi,0)
+}
+  //  newCode.replaceAll("a", 4),
+  // shhh.replaceAll("e", 3),
+  // shhh.replaceAll("i", 1),
+  // shhh.replaceAll("o", 0))
+  //  return newCode
+  
+
+console.log(codex(secretCodeWord1));
+
+
+  // shhh.includes("a", "e", "i", "o")
+  //   let newString = shhh.replaceAll("aeio", "4310") 
+  //   let letter_a = "4"
+  //   const letter_e = "3"
+  //   const letter_i = "1"
+  //   const letter_o = "0"   
+  //     if (shhh.includes("a","e","i","o")){
+  //       return shhh.replaceAll("a","4") ("e","3") 
+  //   }else {
+  //       return "Recalibrate"
+  //   }
+
+
+// only able to get the first A to return as 4. stuck!!!
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
-
-// a) Create a test with expects statement using the variable provided.
-
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+// PsuedoCode: Create the test statement and name it "sorted"
+// Add both given variables to the expect statement because both will be accounted for in the test
+// a) Create a test with expect statements using the variables provided.
+// Create a function for the test and create a function for the method of .filter(which only works on arrays) to filter the value of each array and search for any letters with the includes method.
+//Use the OR operator to include another return condition
+//Use .toUpperCase so that the capital A in apricot is also included in the search for letters a and e.
+describe("sorted", () => {
+    it("returns an array of all the words containing a single particular letter", () => {
+      expect(sorted(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+      expect(sorted(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
+  const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
 const letterA = "a"
 // Expected output: ["Mango", "Apricot", "Peach"]
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
+// First Output: ReferenceError: sorted is not defined
 
 // b) Create the function that makes the test pass.
+const sorted = (newArray, letter) => {
+  return newArray.filter((value) => {
+  return value.includes(letter) ||
+   value.includes(letter.toUpperCase())
+  
+      
+  })
+}
+    // if (letterA === "a") {
+    //     return (["Mango", "Apricot", "Peach"])
+    // } else if (letterE === "e") {
+    //     return ["Cherry", "Blueberry", "Peach"]
+    //   } else  "no no no"
+        
+      
 
+
+ 
+// console.log(sorted(fruitArray.includes(letterA)));
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+// Name it myHand
+// Create a function named pokerHand and a variable within the function in order to use the .sort method on the parameter I created
+// Create conditional statements that will allow the cards to equal each other according to a full house and a pair.
+// The && Oporator will be used to connect the "cards" as equal to the next
+// 
+describe ("myHand", () => {
+it("returns a value of true or false", () => {
+  expect(myHand(hand1)).toEqual(true)
+  expect(myHand(hand2)).toEqual(false)
+  expect(myHand(hand3)).toEqual(false)
+  expect(myHand(hand4)).toEqual(true)
+
+})
+})
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -50,3 +156,20 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+// const placeHolder = (parameter) => {
+// parameter.sort((a,b) => a - b)
+
+// }
+
+
+const myHand = (pokerHand) => {
+  let cards = pokerHand.sort ((a,b) => a - b)
+  if (cards[0] ===[1] && cards [1] === cards [2] && cards [3] === cards[4]){
+  return true;
+  
+}else if (cards[0] === cards[1] && cards[3] && cards [2] === cards[4]){
+  return true;
+  
+  }else 
+    return false
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,65 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: Calling super in ruber allows you to call on parameters from a parent class to a child class using Class Inherritance
 
-Researched answer:
+Researched answer: The arguments of a parent class will be passed down to the child class for use in their super() allowing the code to be used in the subclass without having to re-write it out for each sub-class that is created. For example if the Parent class used 
+class Snacks
+def initialize (name)
+@name=name
+the sub class would use Class Chips < Snacks 
+def initialize(name,flavor)
+super(name)
+@flavor= flavor
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gym is a Ruby frame work or Ruby specific Package manager that allows ruby apps to function
 
-Researched answer:
+Researched answer: A Ruby Gem containts a packaged Ruby application or library, they can be created and shared, they are considered libraries because they have working code within them that can allow your program to run without having to manually input the code themselves. the more common Gems are 1.Bundler which tracks and installs exact gems and versions needed for your program. 2. RSpec which is a testing framework for code. 3. Devise For user authentications and account access for websites. 4. JSON Provides an API for parsing JSON from text. 5. Nokogiri this Provides HTML, XML, SAX, and Reader parsers with XPath and CSS selector support. 6. RAILS!! which is a framework for creating web application with Ruby.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database allows you to pull information using action commands that relate to the information stored within the database and yes there is more than one kind of database
 
-Researched answer:
+Researched answer: Relational databases allows storage and the acess to information that relates to one another, There are three types of relations, One-to-one| many-to-many| one-to-many|
+These databases are great for displaying information in tables. There is alos Object Oriented databases, Hiearachal databases, and Network databases.
+Object Oriented databases - is a combination of relational database concepts and object-oriented principles, best when working with Object-oriented languages
+Hiearachal databases - is most suitable for use cases, There is generally a root “parent” directory of data stored as records links to other subdirectory branches. A parent record can have many child records, each can only have one parent record. Data within records are held in the form of fields, and each field can only hold one value.
+Network databases - numerous member records or files can be connected to multiple owner files and vice versa. Unlike the Hiearchal database the network database model permits each record to have multiple parent and multiple child records.
 
-4. What are primary keys? Why are they important?
 
-Your answer:
+4. What are primary keys? Why are they important? Primary keys are the main 
 
-Researched answer:
+Your answer: A primary key is alike a header in a table. For example if there is a table chart about types of pizza and the top of each column was a "primary Key" something that Identitifes each row below.
+
+Researched answer: The columns within a relational database that acts as the parent to the data under it, It is used as the identifier for the data within the table. they are important because they are used when creating, reading, updating, or deleting data in a database table. You can narrow down more specific points in the table using the Primary Keys.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: Actions being requested for internet connection
 
-Researched answer:
+Researched answer: The HTTP Verbs are request methods to perform actions on an internet protocol server. 
+CONNECT and TRACE belong with "CREATE":
+GET, HEAD, and OPTIONS verbs belong with the "READ":
+POST, PUT, and Patch belon with "Update":
+DELETE belongs to "DELETE":
+
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A set of standards to create reusable routes, designed for HTTP Methods and Crud actions. A cheat sheet for the best pathway to use.
 
-2. Model validations:
+2. Model validations: A set of processes to confirm that a model achieves it's intended purpose. Like a quality assurance checker to make sure that the overall goal is achieved.
 
-3. Params:
+3. Params: Params is an important keyword in C#. It is used as a parameter which can take the variable number of arguments.
+It is useful when programmer don’t have any prior knowledge about the number of parameters to be used.
+Only one Params keyword is allowed and no additional Params will be allowed in function declaration after a params keyword.
+The length of params will be zero if no arguments will be passed.
 
-4. ERB:
+4. ERB: ERB (Embedded RuBy) is a feature of Ruby that enables you to conveniently generate any kind of text, in any quantity, from templates.
+ERB templates combine plain text with Ruby code for variable substitution and flow control, making them easy to write and maintain.
 
-5. API:
+5. API: Application Programming Interface. Interface can be thought of as a contract of service between two applications. This contract defines how the two communicate with each other using requests and responses.
